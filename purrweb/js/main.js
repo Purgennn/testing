4dd09e43ff73d3a84bd12d7532ef11d2e88144e3
purgen <purgenn12@mail.ru>
2022-04-15 15:44:41 +0600

    ready

---
+++ purrweb/js/main.js
@@ -0,0 +1,35 @@
+
+function checkForm(form){
+var sd=[]
+for(i=1;i < 8;i++){
+
+
+var total = sd.push('empt'+i);
+
+if (document.getElementById("empt"+i).value == ''){
+    document.getElementById('empt'+i).style.border = "3px solid red";
+
+    
+}
+else{
+
+    if(document.getElementById("empt"+i).value = ''){
+
+        document.getElementById("button").type="submit";
+
+    }
+                 
+        
+       
+}
+
+
+
+}
+
+}
+
+
+
+
+
